5f6da41dbf893d546c49d7bc62da6e5f610bfccc
arshia adouli <arshiaadouli@gmail.com>
2023-03-09 10:36:30 +1100

    iupac_graph link

--- momoda/settings.py
+++ momoda/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'PORT': os.getenv("PORT"),
 
     }}
-
+print( os.getenv("USER"))
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
